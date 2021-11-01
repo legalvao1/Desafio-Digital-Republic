d622084ffba1c5f1b7f26ce89761d8d3d98cf044
legalvao1 <legalvao1@gmail.com>
2021-10-31 21:59:13 -0300

    Configurando controller para fazer a requisiçao para o model e para renderizar a view

---
+++ controller/index.js
@@ -0,0 +1,20 @@
+const model = require('../models');
+
+const renderizaCalculadora = (_req, res) => {    
+  try{
+    res.render('calculadora', {resultado: '' });
+  } catch(error) {
+    res.render('calculadora', { resultado: error.message });
+  }
+}
+
+const calculadoraDeTinta = (req, res) => {      
+  try{    
+    const resultado = model.paredes(req.body);    
+    res.render('calculadora', { resultado });
+  } catch(error) {
+    res.render('calculadora', { resultado: error.message });
+  }
+}
+
+module.exports = { renderizaCalculadora, calculadoraDeTinta }
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const controller = require('./controller');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+app.get('/', controller.renderizaCalculadora);
+app.post('/', controller.calculadoraDeTinta);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
