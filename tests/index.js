adf8943848d8ebb8ed5b5ae126d260c58a57f84b
legalvao1 <legalvao1@gmail.com>
2021-11-01 16:26:07 -0300

    Testando erro da area da parede

--- tests/index.js
+++ tests/index.js
@@ -31,5 +31,32 @@ describe('Testa a função que recebe as medidas das quatro paredes', () => {
     expect(resposta).to.equals(correctResponseExample)
   });
 
+  it('testa se um erro é retornado, quando area da parede maior que o permitido', () => {
+    const dataExmple = {
+      altura1: '8',
+      largura1: '1',
+      porta1: '1',
+      janela1: '0',
+      altura2: '8',
+      largura2: '1',
+      porta2: '0',
+      janela2: '1',
+      altura3: '8',
+      largura3: '1',
+      porta3: '0',
+      janela3: '0',
+      altura4: '8',
+      largura4: '2',
+      porta4: '0',
+      janela4: '0'
+    };
+
+    const responseExample = 'Área da parede inválida, \nTente novamente'
+    const resposta = paredes(dataExmple);
+
+    expect(resposta).to.equals(responseExample)
+  });
+
 });
+
 
